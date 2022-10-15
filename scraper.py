3c020bfe756daea2e24f3031044a1c774b4faa0d
mkk100 <tommychimes2003@gmail.com>
2022-10-15 16:16:57 -0700

    returnArrays

--- scraper.py
+++ scraper.py
@@ -8,13 +8,15 @@ def find_Upper_Price(rent):
         'https://vancouver.craigslist.org/search/apa?sort=date&')
     soup = BeautifulSoup(html_text.content, 'lxml')
     getPrices = soup.find_all('span', class_='result-price')
+    arr = []
     # print(len(getPrices))
     for prices in getPrices:
         if int(prices.text.replace(',', '').replace('$', '')) <= rent:
-            print(int(prices.text.replace(',', '').replace('$', '')))
+             arr.append(int(prices.text.replace(',', '').replace('$', '')))
       # prices = getPrices.text
         # if (prices != None and int(prices.text) <= rent):
         #   print("here")
+    return arr
 
 
 def housing(bedroom):
@@ -22,11 +24,12 @@ def housing(bedroom):
         'https://vancouver.craigslist.org/search/apa?sort=date&')
     soup = BeautifulSoup(html_text.content, 'lxml')
     getBedroom = soup.find_all('span', class_='housing')
+    arr = []
     # print(len(getBedroom))
     for room in getBedroom:
         #prices = getPrices.text
-        print(room.text.strip()[0])
-
+        arr.append(room.text.strip()[0])
+    return arr
 
 find_Upper_Price(4500)
 #housing(3)
