1f4f93ca15085c8138cb1260436d3df201b33930
mkk100 <>
2022-12-27 15:32:47 -0800

    routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { HistoryComponent } from './history/history.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './main/nav-bar/nav-bar.component';
+const appRoute: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'hist', component:HistoryComponent}
+] 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    HistoryComponent
+    HistoryComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, MainComponent,NavBarComponent]
 })
 export class AppModule { }
